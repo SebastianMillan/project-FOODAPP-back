c20424289d7f10d13aea90e57c6e224a2f708ea4
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-11-17 13:36:44 +0100

    First commit

---
+++ src/main/java/com/salesianostriana/dam/projectFOODAPP/ProjectFoodappApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.projectFOODAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFoodappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFoodappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/dam/projectFOODAPP/ProjectFoodappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.projectFOODAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFoodappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
