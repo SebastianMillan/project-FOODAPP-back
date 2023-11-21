64ad1b70d0e6e716a01d3264b48dbd903316f90b
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-11-21 21:46:18 +0100

    lista trabajadores

--- src/main/java/com/salesianostriana/dam/projectFOODAPP/usuario/service/TrabajadorService.java
+++ src/main/java/com/salesianostriana/dam/projectFOODAPP/usuario/service/TrabajadorService.java
@@ -1,12 +1,19 @@
 package com.salesianostriana.dam.projectFOODAPP.usuario.service;
 
+import com.salesianostriana.dam.projectFOODAPP.usuario.model.Trabajador;
 import com.salesianostriana.dam.projectFOODAPP.usuario.repository.TrabajadorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TrabajadorService {
 
     private final TrabajadorRepository trabajadorRepository;
+
+    public List<Trabajador> findAllTrabajadores(){
+        return trabajadorRepository.findAll();
+    }
 }
