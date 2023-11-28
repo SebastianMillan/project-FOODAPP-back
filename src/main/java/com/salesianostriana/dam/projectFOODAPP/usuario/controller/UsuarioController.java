15af173a2a665b77f5636aa2af134211cdc73555
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2023-11-28 12:34:08 +0100

    Ajustar y arreglar Documentación Open Api y conseguir registrar un Cliente

--- src/main/java/com/salesianostriana/dam/projectFOODAPP/usuario/controller/UsuarioController.java
+++ src/main/java/com/salesianostriana/dam/projectFOODAPP/usuario/controller/UsuarioController.java
@@ -37,23 +37,58 @@ public class UsuarioController {
     private final AuthenticationManager authManager;
     private final JwtProvider jwtProvider;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Registrarme como Cliente", content = {
+                    @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = UserResponse.class)),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            {
+                                                "id": "c0a84001-8c15-1042-818c-15828332000f",
+                                                "username": "luismai",
+                                                "avatar": "https://img.a.transfermarkt.technology/portrait/big/610461-1647594517.jpg?lm=1",
+                                                "fullName": "Luis Miguel",
+                                                "roles": [
+                                                    "CLIENTE"
+                                                ],
+                                                "createdAt": null
+                                            }
+                                            """
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "400", description = "Dato introducido inválido", content = @Content)
+    })
+    @Operation(summary = "createUser", description = "Registrarme como Cliente")
     @PostMapping("/auth/register")
-    public ResponseEntity<UserResponse> createUserWithUserRole(@RequestBody CreateUserRequest createUserRequest) {
-        Usuario user = userService.createUserWithUserRole(createUserRequest);
+    public ResponseEntity<UserResponse> createUserWithUserRole(@Valid @RequestBody CreateClientDto created) {
+        Cliente cliente = userService.createUserWithUserRole(created);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(UserResponse.fromUser(user));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(UserResponse.fromUser(cliente));
     }
 
-    // Más adelante podemos manejar la seguridad de acceso a esta petición
-
-    @PostMapping("/auth/register/admin")
-    public ResponseEntity<UserResponse> createUserWithAdminRole(@RequestBody CreateUserRequest createUserRequest) {
-        Usuario user = userService.createUserWithAdminRole(createUserRequest);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(UserResponse.fromUser(user));
-    }
-
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Logeo", content = {
+                    @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = JwtUserResponse.class)),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            {
+                                                "id": "c0a84001-8c15-1cb9-818c-157d030f000e",
+                                                "username": "luismi",
+                                                "avatar": "https://images.ctfassets.net/86mn0qn5b7d0/featured-img-of-post-147629/b3ae746d78d5df9009780b0f72737d3a/featured-img-of-post-147629.jpg?fm=jpg&fl=progressive&q=50&w=1200&h=900&fit=fill",
+                                                "fullName": "Luis Miguel",
+                                                "roles": [
+                                                    "CLIENTE"
+                                                ],
+                                                "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjMGE4NDAwMS04YzE1LTFjYjktODE4Yy0xNTdkMDMwZjAwMGUiLCJpYXQiOjE3MDExNjc1OTMsImV4cCI6MTcwMTI1Mzk5M30.gEitIh4OuXDBILSplw6IrAk0KU2sXFXMGmItsWWCtqpSiKY8x81WnI2aTwuXQ66RxIuml-uIU9_QcmhGFCKgUA"
+                                            }
+                                            """
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "401", description = "No se encuentra Usuario", content = @Content)
+    })
+    @Operation(summary = "login", description = "logeo")
     @PostMapping("/auth/login")
     public ResponseEntity<JwtUserResponse> login(@RequestBody LoginRequest loginRequest) {
         Authentication authentication =
@@ -68,30 +103,6 @@ public class UsuarioController {
         Usuario user = (Usuario) authentication.getPrincipal();
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(JwtUserResponse.of(user, token));
-    }
-
-    @PutMapping("/user/changePassword")
-    public ResponseEntity<UserResponse> changePassword(@RequestBody ChangePasswordRequest changePasswordRequest,
-                                                       @AuthenticationPrincipal Usuario loggedUser) {
-        // Este código es mejorable.
-        // La validación de la contraseña nueva se puede hacer con un validador.
-        // La gestión de errores se puede hacer con excepciones propias
-        try {
-            if (userService.passwordMatch(loggedUser, changePasswordRequest.getOldPassword())) {
-                Optional<Usuario> modified = userService.editPassword(loggedUser.getId(), changePasswordRequest.getNewPassword());
-                if (modified.isPresent())
-                    return ResponseEntity.ok(UserResponse.fromUser(modified.get()));
-            } else {
-                // Lo ideal es que esto se gestionara de forma centralizada
-                // Se puede ver cómo hacerlo en la formación sobre Validación con Spring Boot
-                // y la formación sobre Gestión de Errores con Spring Boot
-                throw new RuntimeException();
-            }
-        } catch (RuntimeException ex) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password Data Error");
-        }
-
-        return null;
     }
 
     @ApiResponses(value = {
