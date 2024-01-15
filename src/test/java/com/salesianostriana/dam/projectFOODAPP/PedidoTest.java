4616de947bb61a1502b3eae4af695b135055f92a
Pedro Franch Echániz <pedrofe.av@gmail.com>
2024-01-15 19:46:00 +0100

    Intento de Test

--- src/test/java/com/salesianostriana/dam/projectFOODAPP/PedidoTest.java
+++ src/test/java/com/salesianostriana/dam/projectFOODAPP/PedidoTest.java
@@ -1,6 +1,8 @@
 package com.salesianostriana.dam.projectFOODAPP;
 
+import com.salesianostriana.dam.projectFOODAPP.categoria.model.Categoria;
 import com.salesianostriana.dam.projectFOODAPP.pedido.model.EstadoPedido;
+import com.salesianostriana.dam.projectFOODAPP.pedido.model.LineaPedido;
 import com.salesianostriana.dam.projectFOODAPP.pedido.model.Pedido;
 import com.salesianostriana.dam.projectFOODAPP.pedido.repository.PedidoRepository;
 import com.salesianostriana.dam.projectFOODAPP.pedido.service.PedidoService;
@@ -10,18 +12,24 @@ import com.salesianostriana.dam.projectFOODAPP.usuario.model.Cliente;
 import com.salesianostriana.dam.projectFOODAPP.usuario.model.TipoTrabajador;
 import com.salesianostriana.dam.projectFOODAPP.usuario.model.Trabajador;
 import com.salesianostriana.dam.projectFOODAPP.usuario.repository.TrabajadorRepository;
+import org.hibernate.annotations.Any;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
 public class PedidoTest {
 
+
+    UUID uuid = java.util.UUID.randomUUID();
 
     Trabajador cocinero = Trabajador.builder()
             .id(new UUID(1L, 9L))
@@ -34,14 +42,21 @@ public class PedidoTest {
             .tipoTrabajador(TipoTrabajador.REPARTIDOR)
             .build();
 
+    Categoria categoria = Categoria.builder()
+            .id(uuid)
+            .nombre("Bebida")
+            .build();
+
     Producto producto = Producto.builder()
             .id(new UUID(1L, 9L))
             .nombre("Coca-Cola")
             .precio(2.0)
+            .categoria(categoria)
             .build();
 
+
     Cliente cliente = Cliente.builder()
-            .id(new UUID(1L, 9L))
+            .id(uuid)
             .nombre("Jose")
             .build();
 
@@ -70,6 +85,21 @@ public class PedidoTest {
     @Test
     void addProductoToPedidoOpen (){
 
+        Optional<Pedido> pedidoEncontrado = Mockito.doReturn(Optional.of(pedido)).when(pedidoRepository).buscarPedidoAbiertoByClienteId(uuid.toString());
+        Optional <Producto> productoAgregado = Mockito.doReturn(Optional.of(producto)).when(productoRepository).encontrarProductoPorId(uuid.toString());
+
+        Optional<Trabajador> repartidorAsginado = Mockito.doReturn(Optional.of(repartidor)).when(trabajadorRepository).randomSelectRepartidor();
+        Optional<Trabajador> cocineroAsignado = Mockito.doReturn(Optional.of(cocinero)).when(trabajadorRepository).randomSelectCocinero();
+
+        LineaPedido nuevaLineaPedido = LineaPedido.builder()
+                .precioUnitario(productoAgregado.get().getPrecio())
+                .cantidad(1)
+                .producto(productoAgregado.get())
+                .build();
+
+      pedidoEncontrado.get().addLineaPedido(nuevaLineaPedido);
+
+      Assertions.assertEquals(1, pedidoEncontrado.get().getLineasPedido().size());
     }
 
 }
